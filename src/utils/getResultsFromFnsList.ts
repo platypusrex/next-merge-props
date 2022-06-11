fb9d1e0979eebe41bae12eaf8ddf1ea3a0e23faa
Frank Cooke <frankcooke79@gmail.com>
2022-06-11 00:48:43 -0400

    refactor: move short circuit util to separate file

--- src/utils/getResultsFromFnsList.ts
+++ src/utils/getResultsFromFnsList.ts
@@ -1,33 +1,12 @@
+import { orange } from './cliColors';
+import { shouldShortCircuit } from './shouldShortCircuit';
 import type {
   AnyObject,
   Context,
   MergePropsOptions,
   MergePropsOptionsSequential,
   PropsResult,
-  ShortCircuitType,
 } from '../types';
-import { orange } from './cliColors';
-
-const shouldShortCircuit = (
-  result: PropsResult,
-  shortCircuit?: ShortCircuitType
-) => {
-  if (!shortCircuit || shortCircuit === 'never') {
-    return false;
-  }
-
-  const hasRedirect: boolean = 'redirect' in result && !!result?.redirect;
-  const hasNotFound: boolean = 'notFound' in result && !!result?.notFound;
-
-  switch (shortCircuit) {
-    case 'redirect-and-notfound':
-      return hasRedirect || hasNotFound;
-    case 'redirect-only':
-      return hasRedirect;
-    case 'notfound-only':
-      return hasNotFound;
-  }
-};
 
 export const getResultsFromFnsList = async <P = AnyObject>(
   ctx: Context,
