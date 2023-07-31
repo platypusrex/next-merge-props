d32e3b815dde31e4974966c2cca964aaead3680b
Frank Cooke <frankcooke79@gmail.com>
2023-07-30 21:45:01 -0400

    chore: cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   coveragePathIgnorePatterns: ['<rootDir>/test'],
-  // testEnvironment: 'jsdom',
   transformIgnorePatterns: ['node_modules', 'dist'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
